7068f4af15b039d58661c1051473868251cece76
Bravo <jsbravor@udistrital.edu.co>
2026-04-22 10:20:49 -0500

    feat(core): implementar ApiResponse, GlobalExceptionHandler y DTOs base

---
+++ src/main/java/com/modelosgr86e1eq6/proyectofacturacion/dto/auth/LoginRequestDTO.java
@@ -0,0 +1,6 @@
+package com.modelosgr86e1eq6.proyectofacturacion.dto.auth;
+
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/modelosgr86e1eq6/proyectofacturacion/dto/auth/LoginResponseDTO.java
@@ -0,0 +1,6 @@
+package com.modelosgr86e1eq6.proyectofacturacion.dto.auth;
+
+public class LoginResponseDTO {
+    private String token;
+    private String email;
+}
