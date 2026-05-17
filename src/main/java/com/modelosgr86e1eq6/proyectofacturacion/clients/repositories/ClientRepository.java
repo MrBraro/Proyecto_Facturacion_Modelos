91ffa071ddbad97f97e105228d874ec22b70c086
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-05-17 14:05:54 -0500

    Mod: Eliminar anotación @Repository de ClientRepository, pues Spring los detecta automáticamente, y para tener consistencia con el resto de los Repositorys

--- src/main/java/com/modelosgr86e1eq6/proyectofacturacion/clients/repositories/ClientRepository.java
+++ src/main/java/com/modelosgr86e1eq6/proyectofacturacion/clients/repositories/ClientRepository.java
@@ -3,12 +3,9 @@ package com.modelosgr86e1eq6.proyectofacturacion.clients.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.modelosgr86e1eq6.proyectofacturacion.clients.entities.Client;
 import java.util.Optional;
 
- 
-@Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
  
     // Buscar solo clientes activos (usado en listados generales)
