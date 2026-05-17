d44096409364ae9eb64e823dcc390bc45bdd5c88
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-05-17 13:58:54 -0500

    Add: Definicion de estrategia en la generación de ID

--- src/main/java/com/modelosgr86e1eq6/proyectofacturacion/clients/entities/Client.java
+++ src/main/java/com/modelosgr86e1eq6/proyectofacturacion/clients/entities/Client.java
@@ -23,7 +23,7 @@ import java.time.LocalDateTime;
 public class Client {
  
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_client", updatable = false, nullable = false)
     private Integer idClient;
  
